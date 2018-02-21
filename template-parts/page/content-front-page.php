cd36a235a8bf5206ba36882c63329e9559b7e4e8
ryancanhelpyou <ryancanhelpyou@gmail.com>
2018-02-20 20:42:33 -0500

    front page & exhibitions layout adjustments

---
+++ template-parts/page/content-front-page.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Displays content for front page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'twentyseventeen-panel ' ); ?> >
+
+	<?php if ( has_post_thumbnail() ) :
+		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'twentyseventeen-featured-image' );
+
+		// Calculate aspect ratio: h / w * 100%.
+		$ratio = $thumbnail[2] / $thumbnail[1] * 100;
+		?>
+
+		<div class="panel-image" style="background-image: url(<?php echo esc_url( $thumbnail[0] ); ?>);">
+			<div class="panel-image-prop" style="padding-top: <?php echo esc_attr( $ratio ); ?>%"></div>
+		</div><!-- .panel-image -->
+
+	<?php endif; ?>
+
+	<div class="panel-content">
+		<div class="wrap">
+			<!-- .entry-header -->
+
+
+				<?php
+					/* translators: %s: Name of current post */
+					the_content( sprintf(
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+						get_the_title()
+					) );
+				?>
+
+
+				<hr style="margin-top: 4em;" />
+				<?php twentyseventeen_edit_link( get_the_ID() ); ?>
+
+		</div><!-- .wrap -->
+	</div><!-- .panel-content -->
+
+</article><!-- #post-## -->
